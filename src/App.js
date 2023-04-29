cd146ae5faed6c753a423104f76ca79b6cf3b591
malred <13695024677@163.com>
2023-04-29 20:38:46 +0800

    malred:init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import axios from 'axios'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <button onClick={async () => {
+        document.cookie = "Authorization=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODUzNjMzMDgsInN1YiI6NX0.5Bzj7rHpghXqjAxmhlmZQr8XKlUDY-Eprvl7llMyRAY"
+        let res = await axios.get("http://localhost:10101/v1/user/query?username=admin", {
+          withCredentials: true
+        })
+        console.log(res.data);
+      }}>send</button>
+    </div >
+  )
 }
 
-export default App;
+export default App
